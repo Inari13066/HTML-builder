e07eed8b1fdb9a4a52086cbe2f6b8f555af7a95a
Inari13066 <karpova13066@gmail.com>
2022-11-04 14:48:35 +0400

    feat: make task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const { mkdir, readdir } = require("node:fs/promises");
+const { copyFile, constants } = require("node:fs/promises");
+const { resolve, join } = require("node:path");
 
+async function makeDirectory() {
+  const projectFolder = resolve(join(__dirname, "files-copy"));
+  const dirCreation = await mkdir(projectFolder, { recursive: true });
+
+  //   console.log(dirCreation);
+  return dirCreation;
+}
+
+async function copyDirectory() {
+  const files = await readdir(resolve(join(__dirname, "files")));
+  for (const file of files) {
+    // console.log(__dirname, file);
+    copyFile(
+      join(__dirname, "files", file),
+      resolve(join(__dirname, "files-copy", file))
+    );
+  }
+}
+
+makeDirectory().catch(console.error);
+copyDirectory().catch(console.error);
