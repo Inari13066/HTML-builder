082d1610d9e2d2c5674ca8c3fe70961ec9d61054
Inari13066 <devil13066@gmail.com>
2023-05-05 10:55:46 +0300

    fix: fix task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,24 +1,36 @@
 const { mkdir, readdir } = require("node:fs/promises");
-const { copyFile, constants } = require("node:fs/promises");
+const { copyFile } = require("node:fs/promises");
+const { exists, unlink } = require("node:fs");
 const { resolve, join } = require("node:path");
 
 async function makeDirectory() {
-  const projectFolder = resolve(join(__dirname, "files-copy"));
-  const dirCreation = await mkdir(projectFolder, { recursive: true });
+    const projectFolder = resolve(join(__dirname, "files-copy"));
+    // exists(projectFolder, (exists) => {
+    //     unlink(projectFolder, (err) => {
+    //         if (err) console.log(err);
+    //     });
+    // });
 
-  //   console.log(dirCreation);
-  return dirCreation;
+    const dirCreation = await mkdir(projectFolder, { recursive: true });
+
+    //   console.log(dirCreation);
+    return dirCreation;
 }
 
 async function copyDirectory() {
-  const files = await readdir(resolve(join(__dirname, "files")));
-  for (const file of files) {
-    // console.log(__dirname, file);
-    copyFile(
-      join(__dirname, "files", file),
-      resolve(join(__dirname, "files-copy", file))
-    );
-  }
+    const files = await readdir(resolve(join(__dirname, "files")));
+    const oldFiles = await readdir(resolve(join(__dirname, "files-copy")));
+    for (const file of files) {
+        // console.log(__dirname, file);
+        copyFile(join(__dirname, "files", file), resolve(join(__dirname, "files-copy", file)));
+    }
+    for (const file of oldFiles) {
+        if (!files.includes(file)) {
+            unlink(resolve(join(__dirname, "files-copy", file)), (err) => {
+                if (err) console.log(err);
+            });
+        }
+    }
 }
 
 makeDirectory().catch(console.error);
